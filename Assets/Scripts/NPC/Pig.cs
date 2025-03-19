5f0b0a64aa884042d401de95e2473204f42cdfc2
kh277 <jkhcoding@naver.com>
2025-03-19 10:54:58 +0900

    part20 : Add Pig hit sound & effect

--- Assets/Scripts/NPC/Pig.cs
+++ Assets/Scripts/NPC/Pig.cs
@@ -1,54 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Pig : MonoBehaviour
 {
     [SerializeField] private string animalName;     // 동물의 이름
     [SerializeField] private int hp;                // 동물의 체력
     [SerializeField] private float walkSpeed;       // 걷기 속도
+    [SerializeField] private float runSpeed;        // 뛰기 속도
+    private float applySpeed;
 
     // 상태 변수
     private bool isAction;          // 행동 실행 중 여부 판별 
-    private bool isWalking;         // 걷기 중 여부 판별
+    private bool isWalking;         // 걷기 여부 판별
+    private bool isRunning;         // 뛰기 여부 판별
+    private bool isDead;            // 사망 여부 판별
 
     [SerializeField] private float walkTime;        // 걷기 시간
     [SerializeField] private float waitTime;        // 대기 시간
+    [SerializeField] private float runTime;         // 뛰기 시간
     private float currentTime;
 
     // 필요한 컴포넌트
     [SerializeField] private Animator anim;
     [SerializeField] private Rigidbody rigid;
     [SerializeField] private BoxCollider boxCol;
+    private AudioSource theAudio;
+    
+    [SerializeField] private AudioClip[] sound_pig_Normal;
+    [SerializeField] private AudioClip sound_pig_Hurt;
+    [SerializeField] private AudioClip sound_pig_Dead;
 
     private Vector3 direction;
 
-
     void Start()
     {
+        theAudio = GetComponent<AudioSource>();
         currentTime = waitTime;
         isAction = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Move();
-        Rotation();
-        ElapseTime();
+        if (!isDead)
+        {
+            Move();
+            Rotation();
+            ElapseTime();
+        }
     }
 
     private void Move()
     {
-        if (isWalking)
-            rigid.MovePosition(transform.position + (transform.forward * walkSpeed * Time.deltaTime));
+        if (isWalking || isRunning)
+            rigid.MovePosition(transform.position + (transform.forward * applySpeed * Time.deltaTime));
     }
 
     private void Rotation()
     {
-        if (isWalking)
+        if (isWalking || isRunning)
         {
-            Vector3 _rotation = Vector3.Lerp(transform.eulerAngles, direction, 0.01f);
+            Vector3 _rotation = Vector3.Lerp(transform.eulerAngles, new Vector3(0f, direction.y, 0f), 0.01f);
             rigid.MoveRotation(Quaternion.Euler(_rotation));
         }
     }
@@ -66,14 +80,18 @@ public class Pig : MonoBehaviour
     private void ResetStatus()
     {
         isWalking = false;
+        isRunning = false;
         isAction = true;
+        applySpeed = walkSpeed;
         anim.SetBool("Walking", isWalking);
+        anim.SetBool("Running", isRunning);
         direction.Set(0f, Random.Range(0f, 360f), 0f);
         RandomAction();
     }
 
     private void RandomAction()
     {
+        RandomSound();
         int _random = Random.Range(0, 4);       // 대기, 풀뜯기, 두리번, 걷기
 
         if (_random == 0)
@@ -89,22 +107,21 @@ public class Pig : MonoBehaviour
     private void Wait()
     {
         currentTime = waitTime;
-        
-        Debug.Log("1");
+        Debug.Log("wait");
     }
 
     private void Eat()
     {
         currentTime = waitTime;
         anim.SetTrigger("Eat");
-        Debug.Log("2");
+        Debug.Log("eat");
     }
 
     private void Peek()
     {
         currentTime = waitTime;
         anim.SetTrigger("Peek");
-        Debug.Log("3");
+        Debug.Log("peek");
     }
 
     private void TryWalk()
@@ -112,6 +129,58 @@ public class Pig : MonoBehaviour
         isWalking = true;
         anim.SetBool("Walking", isWalking);
         currentTime = walkTime;
-        Debug.Log("4");
+        applySpeed = walkSpeed;
+        Debug.Log("walk");
+    }
+
+    private void Run(Vector3 _targetPos)
+    {
+        direction = Quaternion.LookRotation(transform.position - _targetPos).eulerAngles;
+
+        currentTime = runTime;
+        isWalking = false;
+        isRunning = true;
+        applySpeed = runSpeed;
+        anim.SetBool("Running", isRunning);
+        Debug.Log("run");
+    }
+
+    public void Damage(int _dmg, Vector3 _targetPos)
+    {
+        if (!isDead)
+        {
+            hp -= _dmg;
+            if (hp <= 0)
+            {
+                Dead();
+                return;
+            }
+
+            PlaySE(sound_pig_Hurt);
+            anim.SetTrigger("Hurt");
+            Run(_targetPos);
+        }
+        
+    }
+
+    private void Dead()
+    {
+        PlaySE(sound_pig_Dead);
+        isWalking = false;
+        isRunning = false;
+        isDead = true;
+        anim.SetTrigger("Dead");
+    }
+
+    private void RandomSound()
+    {
+        int _random = Random.Range(0, 3);   // 일상 사운드 3개
+        PlaySE(sound_pig_Normal[_random]);
+    }
+
+    private void PlaySE(AudioClip _clip)
+    {
+        theAudio.clip = _clip;
+        theAudio.Play();
     }
 }
